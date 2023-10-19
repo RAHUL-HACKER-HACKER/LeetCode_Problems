cf393b388dd7f5e8bd738ce71079ed2266e9b692
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-19 13:52:11 +0530

    solution

---
+++ Dota2_Senate.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        int dcount = 0, rcount = 0, dban = 0, rban = 0;
+        int n = senate.length();
+        Queue<Character> q = new LinkedList<>();
+        for(char c : senate.toCharArray()){
+            if(c == 'D') dcount++;
+            else rcount++;
+            q.offer(c);
+        }
+        while(dcount > 0 && rcount > 0){
+            char curr = q.poll();
+            if(curr == 'D'){
+                if(dban > 0){
+                    dban--;
+                    dcount--;
+                }else{
+                    rban++;
+                    q.offer(curr);
+                }
+            }else{
+                if(rban > 0){
+                    rban--;
+                    rcount--;
+                }else{
+                    dban++;
+                    q.offer(curr);
+                }
+            }
+        }
+        return rcount > 0 ? "Radiant" : "Dire";
+    }
+
+ 
+}
